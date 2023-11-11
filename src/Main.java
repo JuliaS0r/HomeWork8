ba0de07f642c8ad0836d9bc567ad7208f04286c3
JuliaS0r <Yulyasor0501@yandex.ru>
2023-11-11 06:19:45 +0300

    проверьте дз по массивам 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,54 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //task1
+        int [] arr = new int [3];
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+
+        double [] arr1 = {1.57, 7.654, 9.986};
+
+        int [] arr2 = {3, 7, 8};
+
+        //task2
+        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(arr1));
+        System.out.println(Arrays.toString(arr2));
+
+        //task3
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.print(arr[i]);
+            if (i != 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+
+        for (int i = arr1.length - 1; i >= 0; i--) {
+            System.out.print(arr1[i]);
+            if (i != 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+
+        for (int i = arr2.length - 1; i >= 0; i--) {
+            System.out.print(arr2[i]);
+            if (i != 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+
+        //task4
+        for (int i = 0; i < arr.length;  i ++) {
+            if (arr[i] % 2 == 1) {
+                arr[i] = arr[i] + 1;
+            }
+        }
+        System.out.print(Arrays.toString(arr));
     }
 }
